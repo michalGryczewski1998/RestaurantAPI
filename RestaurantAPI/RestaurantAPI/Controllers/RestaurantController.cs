508ce7ad1d718b79a2a31c4174803c867dfbb931
Michał Gryczewski <michal12083@outlook.com>
2025-08-05 21:37:08 +0200

    Dodanie podstawowego kontrolera

---
+++ RestaurantAPI/RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Model.DatabaseConnection;
+using RestaurantAPI.Model.Entities;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        {
+            var restaurants = _dbContext.Restaurants.ToList();
+
+            return Ok(restaurants);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> Get([FromRoute] int id)
+        {
+            var restaurant = _dbContext.Restaurants.FirstOrDefault(x => x.Id == id);
+
+            if (restaurant is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurant);
+        }
+    }
+}

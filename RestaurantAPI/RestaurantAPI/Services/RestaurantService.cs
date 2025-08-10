e6c56d43b20f6a3e1b2d02393f5e966b3a254fa7
Michał Gryczewski <michal12083@outlook.com>
2025-08-10 23:10:53 +0200

    HotFix - Stworzenie metod aby uniknac redundancji

--- RestaurantAPI/RestaurantAPI/Services/RestaurantService.cs
+++ RestaurantAPI/RestaurantAPI/Services/RestaurantService.cs
@@ -40,13 +40,12 @@ namespace RestaurantAPI.Services
 
         public RestaurantDto GetById(int id)
         {
-            var restaurant = _dbContext
-                .Restaurants
-                .Include(x => x.Adress)
-                .Include(x => x.Dishes)
-                .FirstOrDefault(x => x.Id == id);
+            _logger.LogWarning($"Restauracja z ID {id} wywołanie metody GetById");
+            var restaurant = GetRestaurantById(id);
 
             if (restaurant == null) return null;
+
+            _logger.LogWarning($"Restauracja z ID {id} została pobrana poprawnie");
 
             var res = _mapper.Map<RestaurantDto>(restaurant);
             return res;
@@ -54,27 +53,34 @@ namespace RestaurantAPI.Services
 
         public IEnumerable<RestaurantDto> GetAll()
         {
-            var restaurants = _dbContext
-                .Restaurants
-                .Include(x => x.Adress)
-                .Include(x => x.Dishes)
-                .ToList();
+            _logger.LogWarning($"Wywołanie metody GetAll");
+
+            var restaurants = GetAllRestaurants();
 
             var restaurantsDto = _mapper.Map<List<RestaurantDto>>(restaurants);
+
+            _logger.LogWarning($"Wywołanie metody GetAll zakończone pomyślnie");
+
             return restaurantsDto;
         }
 
         public int Create(CreateRestaurantDto dto)
         {
+            _logger.LogWarning($"Wywołanie metody Create");
+
             var restaurant = _mapper.Map<Restaurant>(dto);
             _dbContext.Restaurants.Add(restaurant);
             _dbContext.SaveChanges();
+
+            _logger.LogWarning($"Wywołanie metody Create zakończone pomyślnie");
 
             return restaurant.Id;
         }
 
         public bool Update(UpdateRestaurantDto resraurantRequest, int id)
         {
+            _logger.LogWarning($"Wywołanie metody Update dla restauracji {id}");
+
             var restaurant = _dbContext
                 .Restaurants
                 .FirstOrDefault(x => x.Id == id);
@@ -84,7 +90,43 @@ namespace RestaurantAPI.Services
             restaurant.UpdateFrom(resraurantRequest);
             _dbContext .SaveChanges();
 
+            _logger.LogWarning($"Wywołanie metody Update dla restauracji {id} zakończone pomyślnie");
+
             return true;
+        }
+
+        private Restaurant? GetRestaurantById(int Id)
+        {
+            var restaurants = _dbContext
+                .Restaurants
+                .Include(x => x.Adress)
+                .Include(x => x.Dishes)
+                .FirstOrDefault(x => x.Id == Id);
+
+            if (restaurants == null)
+            {
+                return null;
+                _logger.LogWarning($"Nie znaleziono restauracji o {Id}");
+            }
+
+            return restaurants;
+        }
+
+        private List<Restaurant>? GetAllRestaurants()
+        {
+            var restaurants = _dbContext
+                .Restaurants
+                .Include(x => x.Adress)
+                .Include(x => x.Dishes)
+                .ToList();
+
+            if (restaurants == null)
+            {
+                return null;
+                _logger.LogWarning($"Nie pobrano restauracji");
+            }
+
+            return restaurants;
         }
     }
 }

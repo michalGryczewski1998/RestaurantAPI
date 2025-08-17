85beb64b9a6fc2148d93b043b80a2d63124c027d
Michał Gryczewski <michal12083@outlook.com>
2025-08-17 20:46:42 +0200

    Dodanie tabel User i Roles

---
+++ RestaurantAPI/RestaurantAPI/Model/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace RestaurantAPI.Model.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Nationality { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PassworldHash { get; set; }
+
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}

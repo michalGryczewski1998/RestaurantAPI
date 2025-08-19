5b85f1b9c5de31f8e265bde3a2ce63cb9ef19745
Michał Gryczewski <michal12083@outlook.com>
2025-08-19 22:33:46 +0200

    Dodatkowa walidacja

--- RestaurantAPI/RestaurantAPI/Model/Validators/RegisterUserDtoValidator.cs
+++ RestaurantAPI/RestaurantAPI/Model/Validators/RegisterUserDtoValidator.cs
@@ -25,6 +25,27 @@ namespace RestaurantAPI.Model.Validators
             RuleFor(x => x.PhoneNumber)
                 .Matches(@"^\+?[0-9]{9,15}$")
                 .WithMessage("Niepoprawny format numeru telefonu.");
+            RuleFor(x => x.Nationality)
+                .NotEmpty()
+                .NotNull()
+                .Matches(@"^[\p{L}]+$") // tylko litery (Unicode)
+                .WithMessage("Pole nie może być puste, i musi zawierać tylko litery.");
+            RuleFor(x => x.DateOfBirth)
+                .NotEmpty()
+                .NotNull()
+                .Must(c => 
+                {
+                    if(c == default) return false;
+
+                    var dzisiaj = DateTime.Now;
+                    var wiek = dzisiaj.Year - c.Year;
+
+                    if (c.Date > dzisiaj.AddYears(-wiek)) wiek--;
+
+                    return wiek >= 18;
+                })
+                .WithMessage("Musisz mieć co najmniej 18 lat.");
+
         }
 
         public List<Tuple<string, bool>> WalidacjaUzytkownika(User user)

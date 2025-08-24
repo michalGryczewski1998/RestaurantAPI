2de7955118c6c752a08b9606c18a738bb8c93d03
Michał Gryczewski <michal12083@outlook.com>
2025-08-24 22:40:45 +0200

    Autentykacja przez tokeny JWT

---
+++ RestaurantAPI/RestaurantAPI/Authentication/AuthenticationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantAPI.Authentication
+{
+    public class AuthenticationSettings
+    {
+        public string JwtKey { get; set; }
+        public int JwtExpire { get; set; }
+        public string JwtIssuer { get; set; }
+    }
+}

968eb384bfdd104ad3b6d169f64b81d143f5fb7d
Michał Gryczewski <michal12083@outlook.com>
2025-08-26 23:09:20 +0200

    Walidacja modelu logowania

---
+++ RestaurantAPI/RestaurantAPI/Model/Validators/LoginValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using RestaurantAPI.Model.Models;
+
+namespace RestaurantAPI.Model.Validators
+{
+    public class LoginValidator : AbstractValidator<LoginDto>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(1)
+                .WithMessage("Proszę podać adres E-Mail");
+            RuleFor(x => x.Password)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(1)
+            .WithMessage("Proszę podać hasło");
+        }
+    }
+}
